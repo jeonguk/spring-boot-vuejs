a8ada3de52a8eeb0c332d77fb211969347be7ced
barry <barry.allen@kakaobank.com>
2019-01-10 16:19:48 +0900

    controller add

---
+++ backend/src/main/java/com/jeonguk/vuejs/controller/rest/UserController.java
@@ -0,0 +1,54 @@
+package com.jeonguk.vuejs.controller.rest;
+
+import com.jeonguk.vuejs.dto.UserDTO;
+import com.jeonguk.vuejs.entity.User;
+import com.jeonguk.vuejs.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    public static final String HELLO_TEXT = "Hello from Spring Boot Backend!";
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/hello")
+    public Response sayHello() {
+        log.info("GET called on /hello resource");
+        return new Response(HELLO_TEXT);
+    }
+
+    @PostMapping("/user")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User addNewUser (@RequestBody UserDTO request) {
+        User user = new User(request.getFirstName(), request.getLastName());
+        User saveUser = userRepository.save(user);
+
+        log.info(saveUser.toString() + " successfully saved into DB");
+
+        return saveUser;
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable("id") long id) {
+        log.info("Reading user with id " + id + " from database.");
+        return userRepository.findById(id).get();
+    }
+
+    //inner class
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    class Response {
+        private String data;
+    }
+}
